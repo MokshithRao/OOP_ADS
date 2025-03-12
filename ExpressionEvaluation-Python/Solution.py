a9c77c6cf3574747074c0a8fb187c0689075a2bc
MokshithRao <mokshithrao310@gmail.com>
2025-03-12 09:54:45 +0530

    ExpressionEvaluation using custom stack

---
+++ ExpressionEvaluation-Python/Solution.py
@@ -0,0 +1,112 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size_ = 0
+
+    def size(self):
+        return self.size_
+    
+    def is_empty(self):
+        return self.size_ == 0
+    
+    def add_first(self, element):
+        new_node = Node(element)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.size_ += 1
+
+    def remove_first(self):
+        if self.head is None:
+            return None
+        
+        removed = self.head.data
+        self.head = self.head.next
+        self.size_ -= 1
+        return removed
+    
+    def contains(self, e):
+        current = self.head
+        while current:
+            if current.data == e:
+                return True
+            current = current.next
+        return False
+        
+    
+
+
+class Stack:
+    def __init__(self):
+        self.stack = LinkedList()
+
+    def empty(self):
+        return self.stack.is_empty()
+    
+    def peek(self):
+        if self.stack.is_empty():
+            raise IndexError("empty stack")
+        return self.stack.head.data
+    
+    def pop(self):
+        if self.stack.is_empty():
+            raise IndexError("empty stack")
+        else:
+            removed = self.stack.head.data
+            self.stack.remove_first()
+            return removed
+
+    def push(self, item):
+        self.stack.add_first(item)
+        return item
+
+    def search(self, o):
+        current = self.stack.head
+        position = 1
+        while current:
+            if current.data == o:
+                return position
+            current = current.next
+            position += 1
+        return -1
+    
+
+
+
+
+def ExpressionEvaluation(s):
+    stack = Stack()
+    s1 = s.split()
+    
+    for i in s1:
+        if i == ")":
+            a = float(stack.pop())
+            operator = stack.pop()
+            b = float(stack.pop())
+            stack.pop()
+            
+            if operator == "+":
+                stack.push(b + a)
+            elif operator == "-":
+                stack.push(b - a)
+            elif operator == "*":
+                stack.push(b * a)
+            elif operator == "/":
+                stack.push(b / a)
+        else:
+            stack.push(i)
+
+    return stack.pop()
+
+print(ExpressionEvaluation(input()))
+
+
+
